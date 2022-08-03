a1087e2dcadfbefb50bf233ada69f8f103f819fe
Felipe Planes <felaupla@MBP-de-Felipe.fibertel.com.ar>
2022-08-03 10:55:51 -0300

    homework 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,15 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
-}
-
+  let sum = 0;
+  for (let i=0; i < num.length; i++) {
+    sum += num[i] * 2**(num.length - 1 - i);
+  }
+  return sum;
+  }
+  
+//10101010
+  
 function DecimalABinario(num) {
   // tu codigo aca
-
-}
-
-
+    let binario = "";
+    while (num !== 0) { 
+      binario = num%2 + binario;
+       num = Math.floor(num / 2)
+    }
+    return binario
+  }
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
