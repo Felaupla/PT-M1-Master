7037bf27d8067a0a9c9a959c86ca5f4b619705bf
Felipe Planes <felaupla@MBP-de-Felipe.fibertel.com.ar>
2022-08-18 20:37:03 -0300

    homework05ended

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -81,7 +81,38 @@ La clase debe tener los siguientes métodos:
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
 
-function HashTable() {}
+function HashTable() {
+  //es un array de objetos Key-value;
+  this.numBuckets = 35;
+  this.buckets = [];
+}
+HashTable.prototype.hash = function (key) {
+  // 'chris'
+  let sum = 0;
+  for (let i = 0; i < key.length; i++) {
+    sum += key.charCodeAt(i);
+  }
+  return sum % this.numBuckets; // 80%35 = 10
+};
+HashTable.prototype.set = function (key, value) {
+  //revisa si es un string
+  if (typeof key !== "string") throw new TypeError("Keys must be strings");
+  let posArr = this.hash(key);
+  //si la posición del arreglo está vacía, crea un objeto
+  if (this.buckets[posArr] === undefined) {
+    this.buckets[posArr] = {};
+  }
+  this.buckets[posArr][key] = value;
+};
+
+HashTable.prototype.get = function (key) {
+  let posArr = this.hash(key);
+  return this.buckets[posArr][key];
+};
+HashTable.prototype.hasKey = function (key) {
+  let posArr = this.hash(key);
+  return this.buckets[posArr].hasOwnProperty(key);
+};
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
