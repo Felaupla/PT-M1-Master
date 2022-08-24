b9a94ef2ff0da7d2a84e296fb311f60ac742c652
Felipe Planes <felaupla@MacBook-Pro-de-Felipe.local>
2022-08-24 16:46:12 -0300

    homework07

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function factorear(num) {
@@ -7,35 +7,79 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  if (typeof num != "number") {
+    return "Error, el argumento debe ser un numero";
+  }
+  if (num % 1 !== 0) {
+    return "Error, el numero debe ser un entero";
+  }
+  if (num <= 0) {
+    return "Error, el numero debe ser positivo";
+  }
+  let factores = [1];
+  for (let i = 2; i <= num; i++) {
+    while (num % i === 0) {
+      num = num / i;
+      factores.push(i);
+    }
+  }
+  return factores;
 }
+
+console.log(factorear(180));
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
 }
-
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    let actual = array[i];
+    let j = i - 1;
+    while (actual < array[j]) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = actual;
+  }
+  return array;
 }
-
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      let temp = array[i];
+      array[i] = array[min];
+      array[min] = temp;
+    }
+  }
+  return array;
 }
-
-
 // No modificar nada debajo de esta línea
 // --------------------------------
 
